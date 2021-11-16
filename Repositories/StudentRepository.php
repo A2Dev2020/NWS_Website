b61b35b2f4e8488b59e05bdaf8ca5fdb313cbff7
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2021-11-16 11:15:30 +0100

    Ajout dans le repo

--- Repositories/StudentRepository.php
+++ Repositories/StudentRepository.php
@@ -1,11 +1,16 @@
 <?php
 
+require_once("pdo.php");
 require_once('./Student.php');
 
 class StudentRepository
 {
     public static function AddStudent(Student $student)
     {
-
+        $query = GetPDO()->prepare("INSERT INTO students(prenom, age) VALUES (?, ?)");
+        $query->execute([
+            $student->Prenom,
+            $student->Age
+        ]);
     }
 }
--- add.php
+++ add.php
@@ -1,16 +1,15 @@
 <?php
 require_once("pdo.php");
 require_once("Student.php");
+require_once("Repositories/StudentRepository.php");
 
 /*$student = new Student($_POST['name']);
 
 var_dump($student);*/
 
+
 $student = new Student("Nicolas", 27);
 
-$query = $pdo->prepare("INSERT INTO students(prenom, age) VALUES (?, ?)");
-$query->execute([
-    $student->Prenom,
-    $student->Age
-]);
+StudentRepository::AddStudent($student);
+
 ?>
