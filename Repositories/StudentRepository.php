8f0ff451587008df68d04f640987cb05bc3e6497
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2021-11-16 10:32:22 +0100

    ajout

---
+++ Repositories/StudentRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('./Student.php');
+
+class StudentRepository
+{
+    public static function AddStudent(Student $student)
+    {
+
+    }
+}
--- add.php
+++ add.php
@@ -1,7 +1,16 @@
 <?php
+require_once("pdo.php");
 require_once("Student.php");
 
-$student = new Student($_POST['name']);
+/*$student = new Student($_POST['name']);
 
-var_dump($student);
+var_dump($student);*/
+
+$student = new Student("Nicolas", 27);
+
+$query = $pdo->prepare("INSERT INTO students(prenom, age) VALUES (?, ?)");
+$query->execute([
+    $student->Prenom,
+    $student->Age
+]);
 ?>
